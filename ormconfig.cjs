126baf70af6f72f84a0b5d5f71261eccfed81be2
dilekiremozbay <dilekiremozbay@gmail.com>
2022-02-13 18:48:47 +0300

    connect to database url

---
+++ ormconfig.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  type: "mysql",
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+};
